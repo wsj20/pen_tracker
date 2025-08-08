60369efa0428a5247b654cecb5e42827ef651331
Clarke, Will (Student) <clarkew1@lancaster.ac.uk>
2025-08-08 21:34:38 +0100

    Fixed settings for deployment

--- penTracker/penTracker/settings.py
+++ penTracker/penTracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pen-leger.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pen-tracker.onrender.com']
 
 
 # Application definition
